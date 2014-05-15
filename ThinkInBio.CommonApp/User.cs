92377d52c0a615c264cc7a2deab8250b07d612f8
oceannut <oceannut@163.com>
2014-05-16 00:53:21 +0800

    20140515

--- ThinkInBio.CommonApp/User.cs
+++ ThinkInBio.CommonApp/User.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using ThinkInBio.Common.Exceptions;
+
 namespace ThinkInBio.CommonApp
 {
 
@@ -12,30 +14,126 @@ namespace ThinkInBio.CommonApp
     public class User
     {
 
+        #region properties
+
         /// <summary>
-        /// 
+        /// 编号。
         /// </summary>
         public long Id { get; set; }
 
         /// <summary>
-        /// 
+        /// 用户名。
         /// </summary>
-        public string Username { get; set; }
+        public string Username { get; internal set; }
 
         /// <summary>
-        /// 
+        /// 明文验证码。
         /// </summary>
-        public string PlainPwd { get; set; }
+        public string PlainPwd { get; internal set; }
 
         /// <summary>
-        /// 
+        /// 密文验证码。
         /// </summary>
-        public string EncryptPwd { get; set; }
+        public string EncryptPwd { get; internal set; }
 
         /// <summary>
-        /// 
+        /// 电子邮件。
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// 角色。
         /// </summary>
         public IList<long> Roles { get; set; }
+
+        /// <summary>
+        /// 显示姓名。
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// 所属组织或部门。
+        /// </summary>
+        public string Group { get; set; }
+
+        /// <summary>
+        /// 创建时间。
+        /// </summary>
+        public DateTime Creation { get; internal set; }
+
+        /// <summary>
+        /// 修改时间。
+        /// </summary>
+        public DateTime Modification { get; internal set; }
+
+        #endregion
+
+        #region constructors
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public User() { }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="pwd"></param>
+        public User(string username, string pwd)
+        {
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                throw new ArgumentNullException();
+            }
+            this.Username = username;
+            this.PlainPwd = pwd;
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="isUserExisted"></param>
+        /// <param name="action"></param>
+        public void Save(Func<string, bool> isUserExisted, 
+            Action<User> action)
+        {
+            if (string.IsNullOrWhiteSpace(this.Username))
+            {
+                throw new InvalidOperationException();
+            }
+            if (isUserExisted != null && isUserExisted(this.Username))
+            {
+                throw new ObjectAlreadyExistedException(this.Username);
+            }
+            DateTime timeStamp = DateTime.Now;
+            this.Creation = timeStamp;
+            this.Modification = timeStamp;
+            if (action != null)
+            {
+                action(this);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        public void Update(Action<User> action)
+        {
+            if (this.Id == 0 || string.IsNullOrWhiteSpace(this.Username))
+            {
+                throw new InvalidOperationException();
+            }
+            DateTime timeStamp = DateTime.Now;
+            this.Modification = timeStamp;
+            if (action != null)
+            {
+                action(this);
+            }
+        }
 
     }
 
